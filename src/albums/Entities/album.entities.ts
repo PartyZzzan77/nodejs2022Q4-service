c309afccaf68ff9da70f95a0385add7ec12b8d07
Mitya <partyzan327@gmail.com>
2023-02-05 22:17:09 +0300

    feat: identify the Album model

--- src/albums/Entities/album.entities.ts
+++ src/albums/Entities/album.entities.ts
@@ -1,14 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'albums' })
 export class Album {
   @ApiProperty({ example: '3fa85f64-5717-4562-b3fc-2c963f66afa6' })
+  @PrimaryGeneratedColumn()
   id: string;
+
   @ApiProperty({ example: 'Music for the Jilted Generation' })
+  @Column()
   name: string;
 
   @ApiProperty({ example: 1994 })
+  @Column()
   year: number;
 
   @ApiProperty({ example: 'd2992f19-7cac-4a86-9b3d-9d143a42b66d' })
+  @Column({ nullable: true })
   artistId?: string;
 }
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,6 +1,7 @@
 import { ConnectionOptions } from 'typeorm';
 import { User } from './users/Entities/user.entitie';
 import { Track } from './tracks/Entities/track.entitie';
+import { Album } from './albums/Entities/album.entities';
 
 export const ormConfig: ConnectionOptions = {
   type: 'postgres',
@@ -8,6 +9,6 @@ export const ormConfig: ConnectionOptions = {
   username: 'tester',
   password: 'tester',
   database: 'music',
-  entities: [User, Track],
+  entities: [User, Track, Album],
   synchronize: true,
 };
