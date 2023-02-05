ba78f8693ace8cd108e6b652d7b7135ed9c74d69
Mitya <partyzan327@gmail.com>
2023-02-05 22:14:40 +0300

    feat: identify the Track model

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,6 @@
 import { ConnectionOptions } from 'typeorm';
 import { User } from './users/Entities/user.entitie';
+import { Track } from './tracks/Entities/track.entitie';
 
 export const ormConfig: ConnectionOptions = {
   type: 'postgres',
@@ -7,6 +8,6 @@ export const ormConfig: ConnectionOptions = {
   username: 'tester',
   password: 'tester',
   database: 'music',
-  entities: [User],
+  entities: [User, Track],
   synchronize: true,
 };
--- src/tracks/Entities/track.entitie.ts
+++ src/tracks/Entities/track.entitie.ts
@@ -1,21 +1,28 @@
-import { IsString, IsUUID } from 'class-validator';
+import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'tracks' })
 export class Track {
   @ApiProperty({ example: 'd9683e62-ce01-4ac4-80c7-23828cd13792' })
-  @IsUUID()
+  @PrimaryGeneratedColumn()
+  //@IsUUID()
   @IsString({ each: true })
   id: string;
 
   @ApiProperty({ example: 'Breathe' })
+  @Column()
   name: string;
 
   @ApiProperty({ example: '97c8d54a-b5e4-4a71-90b6-3786a5bbc094' })
+  @Column({ nullable: true })
   artistId?: string;
 
   @ApiProperty({ example: '10c8d54a-v5e4-5a71-90b6-3786a5cec073' })
+  @Column({ nullable: true })
   albumId?: string;
 
   @ApiProperty({ example: 214 })
+  @Column()
   duration: number;
 }
