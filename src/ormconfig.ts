f3a7aae9b394294f52f21cf117e06a8615c186e7
Mitya <partyzan327@gmail.com>
2023-02-09 18:28:41 +0300

    feat: add migrations to container

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -18,7 +18,6 @@ export const ormConfig: DataSourceOptions = {
   password,
   database,
   entities: [User, Track, Album, Artist, Favorite],
-  migrations: ['dist/**/migrations/*.js'],
   synchronize: true,
 };
 
