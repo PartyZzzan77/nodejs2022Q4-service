9340331f6eb2b0964b5294da9a8a854be3c64ba2
Mitya <partyzan327@gmail.com>
2023-02-12 16:15:53 +0300

    refactor: synchronize migrations

--- src/ormconfigMigrations.ts
+++ src/ormconfigMigrations.ts
@@ -1,23 +1,25 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import * as process from 'process';
+import 'dotenv/config';
 import { User } from './users/Entities/user.entitie';
 import { Track } from './tracks/Entities/track.entitie';
 import { Album } from './albums/Entities/album.entities';
 import { Artist } from './artists/Entities/atrtist.entities';
-import * as process from 'process';
-import 'dotenv/config';
 import { Favorite } from './favorites/Entities/favorite.entities';
 
 export const username = process.env.POSTGRES_USER;
 export const password = process.env.POSTGRES_PASSWORD;
 export const database = process.env.POSTGRES_DB;
-export const host = process.env.POSTGRES_HOST;
-export const ormConfig: DataSourceOptions = {
+export const host = process.env.POSTGRES_HOST_MIGRATIONS;
+export const ormConfigMigrations: DataSourceOptions = {
   type: 'postgres',
   host,
   username,
   password,
   database,
   entities: [User, Track, Album, Artist, Favorite],
+  migrations: [__dirname, 'dist/**/migrations/*.js'],
+  migrationsRun: true,
 };
 
-export const dataSource = new DataSource(ormConfig);
+export const dataSource = new DataSource(ormConfigMigrations);
