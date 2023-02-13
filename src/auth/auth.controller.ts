bf56fb245fd0ec50846700b2deb009b5be4dbcbc
Mitya <partyzan327@gmail.com>
2023-02-14 00:59:53 +0300

    feat: add openAPI auth endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,15 +12,38 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AddUserDto } from '../users/dto/add-user.dto';
-import { User } from '../users/Entities/user.entitie';
+import { BadRequestUUID, Invalid, User } from '../users/Entities/user.entitie';
 import { Constants } from '../constants';
 import { Tokens } from './types/tokens.interface';
 import { TokenDto } from './dto/token.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { Auth, Unauthorized } from './Entities/auth.entitie';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: AddUserDto })
+  @ApiCreatedResponse({
+    status: 201,
+    type: User,
+    description: 'The user has been created.',
+  })
+  @ApiBadRequestResponse({
+    type: BadRequestUUID,
+    description: 'no login or password, or they are not a strings',
+  })
   @UseInterceptors(ClassSerializerInterceptor)
   @Post('signup')
   @UsePipes(new ValidationPipe())
@@ -28,6 +51,22 @@ export class AuthController {
     return await this.authService.signup(createAuthDto);
   }
 
+  @ApiOperation({ summary: 'Access tokens and updates must be received' })
+  @ApiResponse({
+    status: 200,
+    type: Auth,
+    description: 'Access tokens and updates must be received',
+  })
+  @ApiBody({ type: AddUserDto })
+  @ApiBadRequestResponse({
+    type: BadRequestUUID,
+    description: 'no login or password, or they are not a strings',
+  })
+  @ApiForbiddenResponse({
+    type: Invalid,
+    description:
+      "no user with such login, password doesn't match actual one, etc.",
+  })
   @Post('login')
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
@@ -41,6 +80,23 @@ export class AuthController {
     return await this.authService.generateJWT(user);
   }
 
+  @ApiOperation({
+    summary: 'To get new pair of Access token and Refresh token',
+  })
+  @ApiResponse({
+    status: 200,
+    type: Auth,
+    description: 'Access tokens and updates must be received',
+  })
+  @ApiBody({ type: TokenDto })
+  @ApiUnauthorizedResponse({
+    type: Unauthorized,
+    description: 'no refreshToken in body',
+  })
+  @ApiForbiddenResponse({
+    type: Invalid,
+    description: 'Refresh token is invalid or expired',
+  })
   @Post('refresh')
   @UsePipes(
     new ValidationPipe({
--- src/auth/dto/token.dto.ts
+++ src/auth/dto/token.dto.ts
@@ -1,6 +1,11 @@
 import { IsJWT } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class TokenDto {
+  @ApiProperty({
+    default:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5ZTgxMDE1LWFiZjMtNGZhOC05NTRiLTlmMDkxZDNlNmU1NiIsImxvZ2luIjoiVEVTVF9MT0dJTiIsImlhdCI6MTY3NjMxOTA0MCwiZXhwIjoxNjc2MzIyNjQwfQ.-Z3rcLvk5-36MNaIlEIcqivGNQd03m97Uv0B1h_MKPs',
+  })
   @IsJWT()
   refreshToken: string;
 }
