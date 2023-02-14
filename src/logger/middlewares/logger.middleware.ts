2bb2f088e3e0567c03c6c7663aab9a1c5b77d9e0
Mitya <partyzan327@gmail.com>
2023-02-14 18:13:05 +0300

    feat: listen uncaughtException unhandledRejection

--- src/logger/middlewares/logger.middleware.ts
+++ src/logger/middlewares/logger.middleware.ts
@@ -2,6 +2,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Response, NextFunction } from 'express';
 import { ExpressRequest } from '../../types/express-request.interface';
 import { Logger } from '../logger';
+import * as process from 'process';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
@@ -12,7 +13,19 @@ export class LoggerMiddleware implements NestMiddleware {
   public async use(req: ExpressRequest, res: Response, next: NextFunction) {
     const { url, params, body } = req;
     const { statusCode } = res;
+
+    process.on('uncaughtException', (err) => {
+      this.logger.error(`Uncaught Exception: ${err.message}`, err.stack);
+      process.exit(1);
+    });
+
+    process.on('unhandledRejection', (reason, promise) => {
+      this.logger.error(`Unhandled Rejection: ${reason}`, promise);
+      process.exit(1);
+    });
+
     this.logger.requestLog({ url, params, body, statusCode });
+
     next();
   }
 }
