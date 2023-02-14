3c6476d9d312eb3d75d09729db1319bd0e5a2fa1
Mitya <partyzan327@gmail.com>
2023-02-14 17:59:21 +0300

    feat: implement custom exception filter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseFilters } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './logger/guards/http-exception.filter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getHello(): string {
     return this.appService.getHello();
   }
