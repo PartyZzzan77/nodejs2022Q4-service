d6d35c6be8a8d3e56ae170ca4990e723fa780a51
Mitya <partyzan327@gmail.com>
2023-02-15 14:11:45 +0300

    feat: init logs folder

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,21 +1,33 @@
 import { Injectable, Scope, ConsoleLogger, LogLevel } from '@nestjs/common';
 import * as process from 'process';
 import 'dotenv/config';
+import { appendFileSync, existsSync, mkdir } from 'node:fs';
+import * as path from 'node:path';
 
 const LEVEL = process.env.LOG_LEVEL as LogLevel;
 const LOGS_FOLDER = process.env.LOGS_FOLDER;
 const MAX_FILE_SIZE = +process.env.MAX_LOG_FILE_SIZE;
 
+const logsDir = path.join(LOGS_FOLDER);
+const logDir = path.join(LOGS_FOLDER + '/log');
+const warnDir = path.join(LOGS_FOLDER + '/warn');
+const errorDir = path.join(LOGS_FOLDER + '/error');
+
+const fileSize = MAX_FILE_SIZE;
+const logCounter = 1;
+const warnCounter = 1;
+const errorCounter = 1;
+
 @Injectable({ scope: Scope.TRANSIENT })
 export class Logger extends ConsoleLogger {
-  constructor(
-    private logDir: string = LOGS_FOLDER,
-    private fileSize = MAX_FILE_SIZE,
-    private logCounter = 0,
-    private warnCounter = 0,
-    private errorCounter = 0,
-  ) {
+  constructor() {
     super('App', { logLevels: [LEVEL] });
+    this.setContext('App');
+
+    this.initLogsDir(logsDir);
+    this.initLogsDir(logDir, true, `/${logCounter}.log.txt`);
+    this.initLogsDir(warnDir, true, `/${warnCounter}.log.txt`);
+    this.initLogsDir(errorDir, true, `/${errorCounter}.log.txt`);
 
     process.on('uncaughtException', (err) => {
       this.error(`Uncaught Exception: ${err.message}`, err.stack);
@@ -29,5 +41,24 @@ export class Logger extends ConsoleLogger {
   }
   async log(message: any) {
     super.log(message);
+  }
+
+  private initLogsDir(dist, isFile = false, file = '') {
+    if (!existsSync(dist)) {
+      mkdir(dist, (err) => {
+        if (err) {
+          return;
+        }
+      });
+    }
+    if (isFile) {
+      const fileDist = path.join(dist + file);
+
+      if (existsSync(fileDist)) {
+        return;
+      }
+
+      appendFileSync(fileDist, `\n${new Date().toISOString()} - init`);
+    }
   }
 }
